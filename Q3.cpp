43c60759bdc8d13da8966f5463656f77d4f4a047
abikhameem <42241025+abikhameem@users.noreply.github.com>
2018-08-23 15:42:03 +0000

    Add files via upload

---
+++ Q3.cpp
@@ -0,0 +1,17 @@
+//load the library
+#include<iostream>
+using namespace std;
+//using main function
+int main(){
+//displaying operation
+cout <<"converting 't' from farenheit to celsius" <<endl;
+//declaring variable
+float t;
+cin>>t;
+cout <<"t="<<t<<"farenheit"<<endl;
+//operation in d
+float a=(5*(t-32))/9;
+//displaying result
+cout<<"t="<<a<< "celsius"<<endl;
+return 0;
+}
